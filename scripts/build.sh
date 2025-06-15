27d6ddb9bd0cc8b2a0c322f5f7572d99fd4324da
daily <60293305+daily3014@users.noreply.github.com>
2025-06-15 15:30:36 +0200

    Many stuff
    Added Blake3, Poly1305 + AEAD
    Optimized MD5, Blake2b, SHA384, SHA512, SHA512/t variants, EdDSA
    
    Type checking fixes, updated require method, new testing system by Sythivo, general code cleanup

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,4 @@
 
 set -e
 
-# If Packages aren't installed, install them.
-if [ ! -d "DevPackages" ]; then
-    sh scripts/install-packages.sh
-fi
-
 rojo build default.project.json -o Cryptography.rbxm
--- scripts/dev.sh
+++ scripts/dev.sh
@@ -2,10 +2,5 @@
 
 set -e
 
-# If Packages aren't installed, install them.
-if [ ! -d "DevPackages" ]; then
-    sh scripts/install-packages.sh
-fi
-
 rojo serve dev.project.json \
     & rojo sourcemap dev.project.json -o sourcemap.json --watch
