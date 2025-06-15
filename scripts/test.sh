ab1843e3ed7d999d4bf102917c0a47a26a32332f
daily <120916578+XoifaiI@users.noreply.github.com>
2025-06-15 15:30:36 +0200

    Many stuff
    Added Blake3, Poly1305 + AEAD
    Optimized MD5, Blake2b, SHA384, SHA512, SHA512/t variants, EdDSA
    
    Type checking fixes, updated require method, new testing system by Sythivo, general code cleanup

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,10 +4,5 @@ set -e
 
 OUTPUT=CryptographyTest.rbxl
 
-# If Packages aren't installed, install them.
-if [ ! -d "DevPackages" ]; then
-    sh scripts/install-packages.sh
-fi
-
 rojo build dev.project.json --output $OUTPUT \
     && run-in-roblox --place $OUTPUT --script scripts/run-tests.server.luau
