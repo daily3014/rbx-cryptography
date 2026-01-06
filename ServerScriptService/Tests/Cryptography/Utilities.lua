d4766c04d30c92c86d122de694455149ba6eeff7
Nowoshire <92903766+Nowoshire@users.noreply.github.com>
2026-01-06 23:16:19 +0800

    Updated ServerScriptService/Tests/Cryptography/Utilities.lua

---
+++ ServerScriptService/Tests/Cryptography/Utilities.lua
@@ -0,0 +1,11 @@
+-- @ScriptType: ModuleScript
+--!strict
+
+local Algorithms = table.freeze({
+	RandomString = require("@self/RandomString"),
+	Conversions = require("@self/Conversions"),
+	Base64 = require("@self/Base64"),
+	CSPRNG = require("@self/CSPRNG")
+})
+
+return Algorithms
