418f1aea3b5a806fdabc7c8eb6d26f8850bf1260
Nowoshire <92903766+Nowoshire@users.noreply.github.com>
2026-01-06 23:15:15 +0800

    Updated ServerScriptService/Tests/Cryptography/Hashing/SHA2.lua

---
+++ ServerScriptService/Tests/Cryptography/Hashing/SHA2.lua
@@ -0,0 +1,11 @@
+-- @ScriptType: ModuleScript
+--[[
+	Cryptography library: SHA2 Family  
+--]]
+
+return {
+	SHA224 = require("@self/SHA224"),
+	SHA256 = require("@self/SHA256"),
+	SHA384 = require("@self/SHA384"),
+	SHA512 = require("@self/SHA512"),
+}
