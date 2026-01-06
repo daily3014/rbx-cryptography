a74c3b6ee862a2f2bdf2f9603f26ee16eda51d32
Nowoshire <92903766+Nowoshire@users.noreply.github.com>
2026-01-06 23:16:24 +0800

    Updated ServerScriptService/Tests/Cryptography/Encryption.lua

---
+++ ServerScriptService/Tests/Cryptography/Encryption.lua
@@ -0,0 +1,12 @@
+-- @ScriptType: ModuleScript
+--!strict
+
+local Algorithms = table.freeze({
+	AEAD = require("@self/AEAD"),
+	AES = require("@self/AES"),
+	XOR = require("@self/XOR"),
+	Simon = require("@self/Simon"),
+	Speck = require("@self/Speck")
+})
+
+return Algorithms
