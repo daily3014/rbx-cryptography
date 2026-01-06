2496ec26a6dffe9a5f4d4892e89411a4b4358690
Nowoshire <92903766+Nowoshire@users.noreply.github.com>
2026-01-07 00:02:41 +0800

    refuses to run without .exe

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-rojo serve dev.project.json \
-    & rojo sourcemap dev.project.json -o sourcemap.json --watch
+rojo.exe serve dev.project.json \
+    & rojo.exe sourcemap dev.project.json -o sourcemap.json --watch
